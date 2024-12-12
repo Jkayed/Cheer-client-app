203194aa54b54d07eb03e50e80097213f97b4844
Jamal <119996570+Jkayed@users.noreply.github.com>
2024-12-12 12:52:52 -0500

    changes

--- Cheerleading-groups-app-490a692b152de47cd6037b98c38b054051b5911c/src/components/GroupDetails.jsx
+++ Cheerleading-groups-app-490a692b152de47cd6037b98c38b054051b5911c/src/components/GroupDetails.jsx
@@ -292,7 +292,7 @@ const GroupDetails = () => {
           return cellValue;
       }
     },
-    [group.ownerID] // Dependency to ensure modal state updates correctly
+    [group] 
   );
 
   const renderCellRequests = React.useCallback(

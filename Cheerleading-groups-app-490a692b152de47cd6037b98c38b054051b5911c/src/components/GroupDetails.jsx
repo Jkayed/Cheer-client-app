0b7f4df10a358ae7020b9a502dfc01affb83c3fe
Jamal Kayed <jkayed0@gmail.com>
2024-12-12 02:15:47 -0500

    Update GroupDetails.jsx

--- Cheerleading-groups-app-490a692b152de47cd6037b98c38b054051b5911c/src/components/GroupDetails.jsx
+++ Cheerleading-groups-app-490a692b152de47cd6037b98c38b054051b5911c/src/components/GroupDetails.jsx
@@ -292,7 +292,7 @@ const GroupDetails = () => {
           return cellValue;
       }
     },
-    [group.ownerID] // Dependency to ensure modal state updates correctly
+    [group] // Dependency to ensure modal state updates correctly
   );
 
   const renderCellRequests = React.useCallback(
